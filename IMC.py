507316fad285b3d35b5d88110ea86bbfefcbbdbd
Matheus Cunha <matheucunha0@gmail.com>
2026-09-02 12:24:12 -0300

    Atualização de faixas do IMC

--- IMC.py
+++ IMC.py
@@ -1,3 +1,29 @@
+"""
+Algoritmo simples em Python para classificação de IMC e associação com potenciais
+riscos à saúde. Desenvolvido para aplicar conceitos de lógica de programação e 
+estruturas condicionais 
+"""
+
+import numpy as np 
+import matplotlib.pyplot as pyplot
+
+#eixo x
+alturas = np.linspace(0.60, 2.72, 50)
+
+#eixo y
+baixo_peso  = 18.5 * (alturas ** 2)
+peso_normal = 25 * (alturas ** 2)
+sobrepeso   = 30 * (alturas ** 2)
+obesidade_1 = 35 * (alturas ** 2)
+obesidade_2 = 40 * (alturas ** 2)
+
+plt.figure(figsize=(10,6))
+
+plt.scatter(alturas, x, color="red", marker="0")
+
+print("Índice de Massa Corporal (IMC)\n")
+
+
 peso = input("Informe seu Peso (kg): ")
 altura = input("Informe sua Altura em (Ex: 1.70): ")
 
@@ -6,22 +32,21 @@ altura = float(altura)
 
 imc = peso / (altura ** 2)
 
-print("Seu IMC é:", int(imc))
+print("\nSeu IMC é:", int(imc))
 
 
 if imc < 18.5:
-    print("| Baixo peso | Fadiga, estresse, perda de cabelo1")
-elif imc >= 18.5 and 24.5: 
-    print("| Peso normal | Menor risco de doenças")
-elif imc >= 25 and 29.9: 
-    print("| Sobrepeso | Fadiga, má circulação, varizes")
-elif imc >= 30 and 34.9: 
-    print("| Obesidade Grau I | Diabetes, infarto, angina")
-elif imc >= 35.9 and 39.9: 
-    print("| Obesidade Grau II | Apneia do sono, falta de ar")
+    print("|Baixo peso| Fadiga, estresse, perda de cabelo")
+elif 18.5 <= imc < 25: 
+    print("|Peso normal| Menor risco de doenças")
+elif 25 <= imc < 30: 
+    print("|Sobrepeso| Fadiga, má circulação, varizes")
+elif 30 <= imc < 35: 
+    print("|Obesidade Grau I| Diabetes, infarto, angina")
+elif 35 <= imc < 40: 
+    print("|Obesidade Grau II| Apneia do sono, falta de ar")
 else:
-    print("| Obesidade Grau III | Refluxo, infarto, AVC, dificuldades de locomoção")
-
+    print("|Obesidade Grau III| Refluxo, infarto, AVC, dificuldades de locomoção")
 
 
 
