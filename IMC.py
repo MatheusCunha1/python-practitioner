7bd28c6fe06e27731d0971b4ec226072e60f8721
Matheus Cunha <matheucunha0@gmail.com>
2026-09-02 19:57:35 -0300

    Gráfico de Dispersão

--- IMC.py
+++ IMC.py
@@ -4,11 +4,25 @@ riscos à saúde. Desenvolvido para aplicar conceitos de lógica de programaçã
 estruturas condicionais 
 """
 
+# /// script
+# dependencies = [
+#     "numpy",
+#     "matplotlib",
+# ]
+# ///
+
+
 import numpy as np 
 import matplotlib.pyplot as pyplot
 
+
+pyplot.figure(figsize=(10,6))
+pyplot.title("Gráfico de Dispersão - IMC")
+pyplot.xlabel("Altura")
+pyplot.ylabel("Peso")
+
 #eixo x
-alturas = np.linspace(0.60, 2.72, 50)
+alturas = np.linspace(0.60, 2.72, 100)
 
 #eixo y
 baixo_peso  = 18.5 * (alturas ** 2)
@@ -17,23 +31,32 @@ sobrepeso   = 30 * (alturas ** 2)
 obesidade_1 = 35 * (alturas ** 2)
 obesidade_2 = 40 * (alturas ** 2)
 
-plt.figure(figsize=(10,6))
+pyplot.fill_betweenx(alturas, 0, baixo_peso, color="lightblue", alpha=0.3, label="Abaixo do Peso")
+pyplot.fill_betweenx(alturas, baixo_peso, peso_normal, color="green", alpha=0.4, label="Peso Normal")
+pyplot.fill_betweenx(alturas, peso_normal, sobrepeso, color="yellow", alpha=0.5, label="Sobrepeso")
+pyplot.fill_betweenx(alturas, sobrepeso, obesidade_1, color="orange", alpha=0.6, label="Obesidade Grau I")
+pyplot.fill_betweenx(alturas, obesidade_1, obesidade_2, color="red", alpha=0.7, label="Obesidade Grau II")
 
-plt.scatter(alturas, x, color="red", marker="0")
 
-print("Índice de Massa Corporal (IMC)\n")
-
-
-peso = input("Informe seu Peso (kg): ")
+peso = input("Informe seu Peso (kg): ") 
 altura = input("Informe sua Altura em (Ex: 1.70): ")
 
 peso = float(peso)
 altura = float(altura)
 
+pyplot.scatter(peso, altura, color="red", marker="o", s=100, zorder=5, label="Vc esta aqui")
+
+pyplot.ylim(min(alturas), max(alturas))
+pyplot.xlim(min(baixo_peso), max(obesidade_2))
+
+pyplot.show()
+
+print("Índice de Massa Corporal (IMC)\n")
+
+
 imc = peso / (altura ** 2)
 
 print("\nSeu IMC é:", int(imc))
-
 
 if imc < 18.5:
     print("|Baixo peso| Fadiga, estresse, perda de cabelo")
